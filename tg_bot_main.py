a4470d98883a956a6c8ca1c8443da556caf7b7bb
FilippPogorelskiy <70700099+FilippPogorelskiy@users.noreply.github.com>
2020-10-25 21:34:35 +0300

    Update tg_bot_main.py

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -24,12 +24,14 @@ def get_town_name(town_input):
     #br = wb.Firefox(options=options)
     GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
     CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    global chrome_options
     chrome_options = wb.ChromeOptions()
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = GOOGLE_CHROME_PATH
+    global br
     br = wb.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
     gismeteo_url = 'https://www.gismeteo.ru'
     br.get(gismeteo_url)
