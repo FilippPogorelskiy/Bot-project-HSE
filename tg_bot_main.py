64a3f9e6f2cfeefe8d58eaeefb7a058fd17bd576
FilippPogorelskiy <70700099+FilippPogorelskiy@users.noreply.github.com>
2020-10-25 21:51:36 +0300

    Update tg_bot_main.py

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -26,13 +26,13 @@ def get_town_name(town_input):
     CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     global chrome_options
     chrome_options = wb.ChromeOptions()
-    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('headless')
     chrome_options.binary_location = GOOGLE_CHROME_PATH
     global br
-    br = wb.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+    br = wb.Chrome(CHROMEDRIVER_PATH, chrome_options=chrome_options)
     gismeteo_url = 'https://www.gismeteo.ru'
     br.get(gismeteo_url)
     town_field = br.find_element_by_xpath('//*[@id="js-search"]')
